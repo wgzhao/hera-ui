e19fc85ebcc0b98608c72db4c26b82fe4f31df7f
wgzhao <wgzhao@gmail.com>
2024-11-21 08:57:39 +0800

    update

--- src/views/schedule-center/schedule-list.vue
+++ src/views/schedule-center/schedule-list.vue
@@ -31,129 +31,142 @@
         </div>
       </el-aside>
       <el-main>
-        <el-form :model="form" label-width="auto" v-if="form.id">
-          <el-row>
-            <el-col :span="8">
-              <el-form-item label="任务id:">
-                <el-input v-model="form.id"></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="调度类型:">
-                <el-input
-                  v-model="form.scheduleType"
-                  :formatter="value => scheduleTypeOptions[value]"
-                ></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="所有人:">
-                <el-input v-model="form.owner"></el-input> </el-form-item
-            ></el-col>
-          </el-row>
+        <el-card>
+          <template #header>
+            <div class="card-header">
+              <h3 class="text-center">基本信息</h3>
+            </div>
+          </template>
+          <el-form :model="form" label-width="auto" v-if="form.id">
+            <el-row>
+              <el-col :span="8">
+                <el-form-item label="任务id:">
+                  <el-input v-model="form.id"></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="调度类型:">
+                  <el-input
+                    v-model="form.scheduleType"
+                    :formatter="value => scheduleTypeOptions[value]"
+                  ></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="所有人:">
+                  <el-input v-model="form.owner"></el-input> </el-form-item
+              ></el-col>
+            </el-row>
 
-          <el-row>
-            <el-col :span="8">
-              <el-form-item label="名称:">
-                <el-input v-model="form.name"></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="依赖任务:" v-if="form.scheduleType == 1">
-                <el-input v-model="form.dependencies"></el-input>
-              </el-form-item>
-              <el-form-item label="定时表达式" v-else>
-                <el-input v-model="form.cronExpression"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="8">
-              <el-form-item label="关注人员:">
-                <el-input v-model="form.focusUser"></el-input> </el-form-item
-            ></el-col>
-          </el-row>
+            <el-row>
+              <el-col :span="8">
+                <el-form-item label="名称:">
+                  <el-input v-model="form.name"></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="依赖任务:" v-if="form.scheduleType == 1">
+                  <el-input v-model="form.dependencies"></el-input>
+                </el-form-item>
+                <el-form-item label="定时表达式" v-else>
+                  <el-input v-model="form.cronExpression"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="关注人员:">
+                  <el-input v-model="form.focusUser"></el-input> </el-form-item
+              ></el-col>
+            </el-row>
 
-          <el-row>
-            <el-col :span="8">
-              <el-form-item label="任务类型:">
-                <el-input v-model="form.runType"></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="依赖周期:" v-if="form.scheduleType == 1">
-                <el-input v-model="form.heraDependencyCycle"></el-input>
-              </el-form-item>
-              <el-form-item label="重试次数:" v-else>
-                <el-input v-model="form.configs['roll.back.times']"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="8">
-              <el-form-item label="管理员:">
-                <el-input v-model="form.uidS"></el-input> </el-form-item
-            ></el-col>
-          </el-row>
+            <el-row>
+              <el-col :span="8">
+                <el-form-item label="任务类型:">
+                  <el-input v-model="form.runType"></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="依赖周期:" v-if="form.scheduleType == 1">
+                  <el-input v-model="form.heraDependencyCycle"></el-input>
+                </el-form-item>
+                <el-form-item label="重试次数:" v-else>
+                  <el-input
+                    v-model="form.configs['roll.back.times']"
+                  ></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="管理员:">
+                  <el-input v-model="form.uidS"></el-input> </el-form-item
+              ></el-col>
+            </el-row>
 
-          <el-row>
-            <el-col :span="8">
-              <el-form-item label="自动调度:">
-                <el-switch
-                  v-model="form.auto"
-                  active-value="开启"
-                  inactive-value="1"
-                ></el-switch> </el-form-item
-            ></el-col>
-            <el-col :span="8"
-              ><el-form-item label="重试次数:" v-if="form.schduleType == 0">
-                <el-input
-                  v-model="form.configs['roll.back.times']"
-                ></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="重复执行:">
-                <el-switch v-model="form.repeatRun"></el-switch> </el-form-item
-            ></el-col>
-          </el-row>
+            <el-row>
+              <el-col :span="8">
+                <el-form-item label="自动调度:">
+                  <el-switch
+                    v-model="form.auto"
+                    active-value="开启"
+                    inactive-value="1"
+                  ></el-switch> </el-form-item
+              ></el-col>
+              <el-col :span="8"
+                ><el-form-item label="重试次数:" v-if="form.schduleType == 0">
+                  <el-input
+                    v-model="form.configs['roll.back.times']"
+                  ></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="重复执行:">
+                  <el-switch
+                    v-model="form.repeatRun"
+                  ></el-switch> </el-form-item
+              ></el-col>
+            </el-row>
 
-          <el-row>
-            <el-col :span="8">
-              <el-form-item label="任务优先级:">
-                <el-input
-                  v-model="form.configs['run.priority.level']"
-                ></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="重试间隔:">
-                <el-input
-                  v-model="form.configs['roll.back.wait.time']"
-                ></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="8">
-              <el-form-item label="机器组:">
-                <el-input
-                  v-model="form.hostGroupName"
-                ></el-input> </el-form-item
-            ></el-col>
-          </el-row>
+            <el-row>
+              <el-col :span="8">
+                <el-form-item label="任务优先级:">
+                  <el-input
+                    v-model="form.configs['run.priority.level']"
+                  ></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="重试间隔:">
+                  <el-input
+                    v-model="form.configs['roll.back.wait.time']"
+                  ></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="机器组:">
+                  <el-input
+                    v-model="form.hostGroupName"
+                  ></el-input> </el-form-item
+              ></el-col>
+            </el-row>
 
-          <el-row>
-            <el-col :span="16">
-              <el-form-item label="描述:">
-                <el-input v-model="form.description"></el-input> </el-form-item
-            ></el-col>
-            <el-col :span="8">
-              <el-form-item label="预计时长:">
-                <!-- <el-input v-model="form.estimatedTime"></el-input> -->
-              </el-form-item></el-col
-            >
-          </el-row>
+            <el-row>
+              <el-col :span="16">
+                <el-form-item label="描述:">
+                  <el-input
+                    v-model="form.description"
+                  ></el-input> </el-form-item
+              ></el-col>
+              <el-col :span="8">
+                <el-form-item label="预计时长:">
+                  <!-- <el-input v-model="form.estimatedTime"></el-input> -->
+                </el-form-item></el-col
+              >
+            </el-row>
 
-          <el-row>
-            <el-col :span="24">
-              <!-- <el-input v-model="form.script" autosize type="area" /> -->
-              <codemirror
-                v-model="form.script"
-                :tabSize="2"
-                :extensions="extensions"
-              />
-            </el-col>
-          </el-row>
-        </el-form>
+            <el-row>
+              <el-col :span="24">
+                <!-- <el-input v-model="form.script" autosize type="area" /> -->
+                <codemirror
+                  v-model="form.script"
+                  :tabSize="2"
+                  :extensions="extensions"
+                />
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-card>
       </el-main>
     </el-container>
   </div>
