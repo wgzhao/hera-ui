2e75814a39fc7272ffa998fc7be1b906e22765bc
wgzhao <wgzhao@gmail.com>
2024-11-22 13:11:54 +0800

    extract tree to single component

---
+++ src/views/schedule-center/components/JobTree.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { ElTree } from "element-plus";
+import { scheduleJob } from "./_data";
+interface Tree {
+  label: string;
+  children?: Tree[];
+}
+defineOptions({
+  name: "JobTree"
+});
+
+const defaultProps = {
+  children: "children",
+  label: "label"
+};
+const zNodes = ref({});
+
+const treeRef = ref<InstanceType<typeof ElTree>>();
+
+function handleNodeClick(data: Tree) {
+  if (data.children?.length == 0) {
+    // click leaf node
+    operateType.value = "jobOperate";
+    const jobId = parseInt(data.label.split("(")[1].slice(0, -1));
+    getJobMessage(jobId).then(res => {
+      form.value = res;
+      if (res.scheduleType == 0) {
+        jobType.value = "cron";
+      } else {
+        jobType.value = "dep";
+      }
+    });
+    console.log(form.value);
+  } else {
+    // click inner node
+    operateType.value = "groupOperate";
+  }
+}
+
+function filterNode(value: string, data: Tree) {
+  if (!value) return true;
+  return data.label.includes(value);
+}
+
+function searchNode() {
+  //filterNode(search.value, scheduleJob);
+  treeRef.value!.filter(search.value);
+}
+
+// watch(search, val => {
+//   treeRef.value!.filter(val);
+// });
+</script>
+
+<template>
+  <div>
+    <el-row class="mt-2">
+      <el-col :span="24">
+        <el-input
+          v-model="search"
+          placeholder="请输入关键词(空格分割,回车搜索)"
+          @keyup.enter="searchNode"
+        />
+      </el-col>
+    </el-row>
+    <el-tree
+      ref="treeRef"
+      class="filter-tree"
+      style="max-width: 600px"
+      :data="scheduleJob"
+      :props="defaultProps"
+      :filter-node-method="filterNode"
+      :expand-on-click-node="false"
+      @node-click="handleNodeClick"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/schedule-center/schedule-list.vue
+++ src/views/schedule-center/schedule-list.vue
@@ -9,27 +9,7 @@
             size="default"
           />
         </div>
-        <el-row class="mt-2">
-          <el-col :span="24">
-            <el-input
-              v-model="search"
-              placeholder="请输入关键词(空格分割,回车搜索)"
-              @keyup.enter="searchNode"
-            />
-          </el-col>
-        </el-row>
-        <div>
-          <el-tree
-            ref="treeRef"
-            class="filter-tree"
-            style="max-width: 600px"
-            :data="scheduleJob"
-            :props="defaultProps"
-            :filter-node-method="filterNode"
-            :expand-on-click-node="false"
-            @node-click="handleNodeClick"
-          />
-        </div>
+        <JobTree />
       </el-aside>
       <el-main>
         <div class="mb-2">
@@ -69,21 +49,18 @@
 </template>
 <script setup lang="ts">
 import { ref, onMounted, watch, computed } from "vue";
-import { ElTree } from "element-plus";
+
 import type { PlusColumn, FieldValues } from "plus-pro-components";
 import { Codemirror } from "vue-codemirror";
 import { useDark } from "@pureadmin/utils";
 import { sql } from "@codemirror/lang-sql";
 import { initJobTree, getJobMessage } from "@/api/schedule-center";
 import type { HeraJob } from "@/api/schedule-center";
-import { scheduleJob } from "./_data";
+
 import { depColumn, cronColumn } from "./_heraJobColumn";
 import JobInfoShow from "./components/JobInfoShow.vue";
 import OperateButton from "./components/OperateButton.vue";
-interface Tree {
-  label: string;
-  children?: Tree[];
-}
+import JobTree from "./components/JobTree.vue";
 
 const scheduleOptions = ["我的调度", "全部调度"];
 const schduleSelect = ref("我的调度");
@@ -92,31 +69,9 @@ const operateType = ref();
 const enableEdit = ref(false);
 const search = ref("");
 
-const defaultProps = {
-  children: "children",
-  label: "label"
-};
-
 const form = ref<FieldValues>({});
 
-const zNodes = ref({});
-
-const treeRef = ref<InstanceType<typeof ElTree>>();
-
 const extensions = [sql()];
-
-function filterNode(value: string, data: Tree) {
-  if (!value) return true;
-  return data.label.includes(value);
-}
-
-function searchNode() {
-  //filterNode(search.value, scheduleJob);
-  treeRef.value!.filter(search.value);
-}
-// watch(search, val => {
-//   treeRef.value!.filter(val);
-// });
 
 const columns = computed(() => {
   if (jobType == "dep") {
@@ -126,25 +81,6 @@ const columns = computed(() => {
   }
 });
 
-function handleNodeClick(data: Tree) {
-  if (data.children?.length == 0) {
-    // click leaf node
-    operateType.value = "jobOperate";
-    const jobId = parseInt(data.label.split("(")[1].slice(0, -1));
-    getJobMessage(jobId).then(res => {
-      form.value = res;
-      if (res.scheduleType == 0) {
-        jobType.value = "cron";
-      } else {
-        jobType.value = "dep";
-      }
-    });
-    console.log(form.value);
-  } else {
-    // click inner node
-    operateType.value = "groupOperate";
-  }
-}
 onMounted(() => {
   // initJobTree().then(res => {
   //   zNodes.value = res.data;
